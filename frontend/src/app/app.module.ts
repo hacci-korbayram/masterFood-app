dd067d72a9ad956a9d3e9b65e3dc4209ab83f679
hacci <hacci_mk@hotmail.com>
2022-10-17 01:46:43 +0200

    cart page editing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent, FoodPageComponent, CartPageComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent, FoodPageComponent, CartPageComponent, TitleComponent],
   imports: [BrowserModule, AppRoutingModule, NgxStarsModule],
   providers: [],
   bootstrap: [AppComponent],
