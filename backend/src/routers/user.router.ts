eee9de735242fec1e635008944c20ff972a200a0
hacci <hacci_mk@hotmail.com>
2022-11-27 22:24:40 +0100

    register added

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -3,7 +3,9 @@ import { sample_users } from "../data";
 import jwt from "jsonwebtoken";
 const router = Router();
 import asyncHandler from "express-async-handler";
-import { UserModel } from "../models/user.model";
+import { User, UserModel } from "../models/user.model";
+import { HTTP_BAD_REQUEST } from "../constants/http.status";
+import bcrypt from "bcryptjs";
 
 router.get(
   "/seed",
@@ -28,8 +30,33 @@ router.post(
     if (user) {
       res.send(generateTokenResponse(user));
     } else {
-      res.status(400).send("user name or password is not valid!");
+      res.status(HTTP_BAD_REQUEST).send("user name or password is not valid!");
     }
+  })
+);
+
+router.post(
+  "/register",
+  asyncHandler(async (req, res) => {
+    const { name, email, password, address } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      res.status(HTTP_BAD_REQUEST).send("user is already exist, please login!");
+      return;
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const newUser: User = {
+      id: "",
+      name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+      address,
+      isAdmin: false,
+    };
+    const dbUser = await UserModel.create(newUser);
+    res.send(generateTokenResponse(dbUser));
   })
 );
 
@@ -50,3 +77,28 @@ const generateTokenResponse = (user: any) => {
 };
 
 export default router;
+
+// const generateTokenReponse = (user: User) => {
+//   const token = jwt.sign(
+//     {
+//       id: user.id,
+//       email: user.email,
+//       isAdmin: user.isAdmin,
+//     },
+//     process.env.JWT_SECRET!,
+//     {
+//       expiresIn: "30d",
+//     }
+//   );
+
+//   return {
+//     id: user.id,
+//     email: user.email,
+//     name: user.name,
+//     address: user.address,
+//     isAdmin: user.isAdmin,
+//     token: token,
+//   };
+// };
+
+// export default router;
--- frontend/src/app/components/partials/input-validation/input-validation.component.ts
+++ frontend/src/app/components/partials/input-validation/input-validation.component.ts
@@ -10,7 +10,10 @@ import { AbstractControl } from '@angular/forms';
 const VALIDATORS_MESSAGES: any = {
   required: 'Should not be empty',
   email: 'Email is not valid',
+  minlength: 'Field is too short',
+  notMatch: 'Password and Confirm does not match',
 };
+
 @Component({
   selector: 'input-validation',
   templateUrl: './input-validation.component.html',
@@ -23,24 +26,27 @@ export class InputValidationComponent implements OnInit, OnChanges {
   showErrorsWhen: boolean = true;
   errorMessages: string[] = [];
   constructor() {}
+
   ngOnChanges(changes: SimpleChanges): void {
-    this.chechkValidation();
+    this.checkValidation();
   }
 
   ngOnInit(): void {
     this.control.statusChanges.subscribe(() => {
-      this.chechkValidation();
+      this.checkValidation();
     });
     this.control.valueChanges.subscribe(() => {
-      this.chechkValidation();
+      this.checkValidation();
     });
   }
-  chechkValidation() {
+
+  checkValidation() {
     const errors = this.control.errors;
     if (!errors) {
       this.errorMessages = [];
       return;
     }
+
     const errorKeys = Object.keys(errors);
     this.errorMessages = errorKeys.map((key) => VALIDATORS_MESSAGES[key]);
   }
